7f04fc9d39482d9bbecca24b6888a244dbe16938
reahwy66-cloud <reahwy66@gmail.com>
2026-09-20 16:33:00 +0300

    Load universal admin spacing stack

--- push.js
+++ push.js
@@ -6,11 +6,11 @@ var PUSH_API="https://studio-ledger-mcp.reahwy66.workers.dev";
 (function mobileUiBootstrap(){
   var link=document.createElement("link");
   link.rel="stylesheet";
-  link.href="mobile.css?v=20260920-v36";
+  link.href="mobile.css?v=20260920-v37";
   document.head.appendChild(link);
   var desktopLink=document.createElement("link");
   desktopLink.rel="stylesheet";
-  desktopLink.href="desktop.css?v=20260920-v37";
+  desktopLink.href="desktop.css?v=20260920-v38";
   document.head.appendChild(desktopLink);
 
   var ICONS={
